f3968833e0d4de6eae2ad4af89d558aa5ffaae04
Khoirulfata <125975178+Khoirulfata@users.noreply.github.com>
2023-11-25 12:45:05 +0700

    Update config.php

--- catering/inc/config.php
+++ catering/inc/config.php
@@ -33,32 +33,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 }
- 
-// Inisialisasi variabel
-$email = $password = "";
-
-// Periksa apakah form sudah dikirim
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Ambil data dari formulir
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    // Query untuk memeriksa keberadaan pengguna di database
-    $sql = "SELECT * FROM pengguna WHERE email='$email' AND password='$password'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-         // Login berhasil, ambil informasi pengguna dan set session
-         $row = $result->fetch_assoc();
-         session_start();
-         $_SESSION['nama'] = $row['nama']; // Gunakan nama sebagai username pada Navbar
-         $_SESSION['email'] = $email; // Simpan email dalam session (sesuai kebutuhan)
-         header("Location: index.php");
-         exit();
-    } else {
-        echo "Login gagal. Periksa kembali username dan password Anda.";
-    }
-}
 // Tutup koneksi
 $conn->close();
 ?>
