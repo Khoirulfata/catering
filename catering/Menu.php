4224e5fac311f693e7b4d3d82d4c59173f174576
Khoirulfata <125975178+Khoirulfata@users.noreply.github.com>
2023-11-26 14:44:13 +0700

    Update Menu.php

--- catering/Menu.php
+++ catering/Menu.php
@@ -1,48 +1,64 @@
 <?php 
-    include"inc/config2.php";
 	include"layout/header.php";
 ?>
     <!-- content -->
-    <div class="diva">
+    <div>
         <ul class="daftar">
             <li class="isi">Daftar Semua menu</li>
         </ul>
-        <ul class="makanan">
-            <li>
-                <img src="1.png" class="gambar" widht="150px" height="150px" /><br>
-                <p>Steak Arab</p>
-                <p>Harga:Rp 12.000</p>
-                <button type="submit" name="submit" style="background-color: blue;color:white;
-                     padding: 10px;">Lihat Detail</button>
-                <button type="submit" name="submit" style="background-color: red;color:white;
-                padding: 10px;">Pesan</button>
-            </li>
-            <li>
-                <img src="4.jpg" class="gambar" widht="150px" height="150px" /> 
-                <p>Teh botol Sosro</p>
-                <p>Harga:Rp 5.000</p>
-                <button type="submit" name="submit" style="background-color: blue;color:white;
-                     padding: 10px;">Lihat Detail</button>
-                <button type="submit" name="submit" style="background-color: red;color:white;
-                padding: 10px;">Pesan</button>
-            </li>
+        <?php
+// Menghubungkan ke database (gantilah dengan informasi koneksi Anda)
+$koneksi = mysqli_connect("localhost", "root", "", "ppl");
 
-            <li>
-                <img src="3.png" class="gambar" widht="150px" height="150px" /> 
-                <p>Salad Buah</p>
-                <p>Harga:Rp 12.000</p>
-                <button type="submit" name="submit" style="background-color: blue;color:white;
-                     padding: 10px;">Lihat Detail</button>
-                <button type="submit" name="submit" style="background-color: red;color:white;
-                padding: 10px;">Pesan</button>
-            </li>
-            <li class="Rp">
-                <p>Rp 0,00</p>
-                <a href="#" class="lide">Lihat Detail</p>
-            </li>
-        </ul>
-        
-    </div>
+// Memeriksa koneksi
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+// Membuat query SELECT untuk mengambil data produk dengan urutan ID produk dari yang terkecil
+$query = "SELECT id, nama, gambar, harga FROM produk ORDER BY id ASC";
+$result = mysqli_query($koneksi, $query);
+
+// Memeriksa apakah query berhasil dieksekusi
+if ($result) {
+    // Memeriksa apakah terdapat data produk
+    if (mysqli_num_rows($result) > 0) {
+        // Menampilkan data produk dalam format yang diinginkan
+        echo "<div style='display: flex; flex-wrap: wrap; justify-content: center;'>";
+        $counter = 0;
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($counter % 4 == 0 && $counter != 0) {
+                // Baris baru setiap empat produk, kecuali untuk produk pertama
+                echo "</div><div style='display: flex; flex-wrap: wrap; padding-left:195px;'>";
+            }
+
+            echo "<div style='margin: 20px;'>
+                    <img src='data:image/jpeg;base64," . base64_encode($row['gambar']) . "' width='200' height='150' alt='Gambar Produk'>
+                    <h2>{$row['nama']}</h2>
+                    <p>Harga: {$row['harga']}</p>
+                    <button style='background-color: blue; color: white;'>Lihat Detail</button>
+                    <button style='background-color: red; color: white; float: right;'>Pesan</button>
+                </div>";
+
+            $counter++;
+        }
+
+        echo "</div>";
+        echo "<div style='position: fixed; bottom: 20px; right: 20px; background-color: red; color: white; padding: 10px;'>
+                    <p>Rp.0.00</p>
+                    <a href='#'style='text-decoration:none;color:white;'>Lihat Detail</a>
+                  </div>";
+    } else {
+        echo "Tidak ada data produk.";
+    }
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
+}
+
+// Menutup koneksi
+mysqli_close($koneksi);
+?>
     <!-- end content -->
 <?php 
 	include"layout/footer.html";
